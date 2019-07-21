d07af6c7d63c026ae53703b41d5985b660ca8d7d
Tiffany <audyc@DESKTOP-OQAFHL3>
2019-07-20 21:20:22 -0600

    Piramide invertida correccion

--- piramide invertida/src/piramide/invertida/PiramideInvertida.java
+++ piramide invertida/src/piramide/invertida/PiramideInvertida.java
@@ -15,6 +15,7 @@ public class PiramideInvertida {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         System.out.println("*********");
         System.out.println(" *******");
         System.out.println("  *****");
