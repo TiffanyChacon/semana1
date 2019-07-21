7ad40a4c8ec342d5c66bf5f3d3f2207d7cb2d3ba
Tiffany <audyc@DESKTOP-OQAFHL3>
2019-07-20 21:08:03 -0600

    asignatura

---
+++ asignaturas/src/asignaturas/Asignaturas.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asignaturas;
+
+/**
+ *
+ * @author Tiffany
+ */
+public class Asignaturas {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       System.out.print("fisica2\t\t 7:00 a 9:00. \nmate discreta\t 9:00 a 11:00. \nprogramación 2\t 11:00 a 13:00"
+                + "\ncalculo 2\t 14:00 a 16:00. \nestadistica 1\t 16:00 a 18:00");
+    }
+    
+}
