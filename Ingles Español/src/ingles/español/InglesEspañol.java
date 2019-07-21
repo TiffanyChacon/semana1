cc59b86c2ee692d26d7343226bb9d7c0361a3a93
Tiffany <audyc@DESKTOP-OQAFHL3>
2019-07-20 21:10:21 -0600

    traduccion ingles español

---
+++ Ingles Español/src/ingles/español/InglesEspañol.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ingles.español;
+
+/**
+ *
+ * @author Tiffany
+ */
+public class InglesEspañol {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("headache              \tdolor de cabeza");
+        System.out.println("stomachache            \tdolor de estomago");
+        System.out.println("the flu                 \t gripe");
+        System.out.println("throw up                 \t vomitar");
+        System.out.println("hurt                      \t lastimar ");
+        System.out.println("air traffic controler      \t control trafico aereo");
+        System.out.println("laugh                       \t risa");
+        System.out.println("vet                          \t veterinario");
+        System.out.println("knee                          \t rodilla");
+        System.out.println("travel                         \t viaje");
+    }
+    
+}
