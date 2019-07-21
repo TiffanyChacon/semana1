aacbdce93d9eb83c71a65e818c7ee66644ed48a5
Tiffany <>
2019-07-20 21:00:47 -0600

    Escena

---
+++ Escena/src/escena/Escena.java
@@ -0,0 +1,48 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package escena;
+
+/**
+ *
+ * @author Tiffany
+ */
+public class Escena {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("                                   ,,,                         MM .M");
+System.out.println("                               ,!MMMMMMM!,                     MM MM  ,.");
+System.out.println("       ., .M                .MMMMMMMMMMMMMMMM.,          'MM.  MM MM .M'");
+System.out.println("     . M: M;  M          .MMMMMMMMMMMMMMMMMMMMMM,          'MM,:M M'!M'");
+System.out.println("    ;M MM M: .M        .MMMMMMMMMMMMMMMMMMMMMMMMMM,         'MM'...'M");
+System.out.println("     M;MM;M :MM      .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.       .MMMMMMMM");
+System.out.println("     'M;M'M MM      MMMMMM  MMMMMMMMMMMMMMMMM  MMMMMM.    ,,M.M.'MMM'");
+System.out.println("      MM'MMMM      MMMMMM @@ MMMMMMMMMMMMMMM @@ MMMMMMM.'M''MMMM;MM'");
+System.out.println("     MM., ,MM     MMMMMMMM  MMMMMMMMMMMMMMMMM  MMMMMMMMM      '.MMM");
+System.out.println("     'MM;MMMMMMMM.MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.      'MMM");
+System.out.println("      ''.'MMM'  .MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM       MMMM");
+System.out.println("       MMC      MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM.      'MMMM");
+System.out.println("      .MM      :MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM''MMM       MMMMM");
+System.out.println("      MMM      :M  'MMMMMMMMMMMMM.MMMMM.MMMMMMMMMM'.MM  MM:M.    'MMMMM");
+System.out.println("     .MMM   ...:M: :M.'MMMMMMMMMMMMMMMMMMMMMMMMM'.M''   MM:MMMMMMMMMMMM'");
+System.out.println("    AMMM..MMMMM:M.    :M.'MMMMMMMMMMMMMMMMMMMM'.MM'     MM''''''''''''");
+System.out.println("    MMMMMMMMMMM:MM     'M'.M'MMMMMMMMMMMMMM'.MC'M'     .MM");
+System.out.println("     '''''''''':MM.       'MM!M.'M-M-M-M'M.'MM'        MMM");
+System.out.println("                MMM.            'MMMM!MMMM'            .MM");
+System.out.println("                 MMM.             '''   ''            .MM'");
+System.out.println("                  MMM.                               MMM'");
+System.out.println("                   MMMM            ,.J.JJJJ.       .MMM'");
+System.out.println("                    MMMM.       'JJJJJJJ'JJJM   CMMMMM");
+System.out.println("                      MMMMM.    'JJJJJJJJ'JJJ .MMMMM'");
+System.out.println("                        MMMMMMMM.'  'JJJJJ'JJMMMMM'");
+System.out.println("                          'MMMMMMMMM'JJJJJ JJJJJ'");
+System.out.println("                             ''MMMMMMJJJJJJJJJJ'");
+System.out.println("                                     'JJJJJJJJ'");
+    }
+    
+}
