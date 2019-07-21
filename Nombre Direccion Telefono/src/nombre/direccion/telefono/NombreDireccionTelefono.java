b2e5e7cffb6675649df30068ee67bbb686c38e41
Tiffany <audyc@DESKTOP-OQAFHL3>
2019-07-20 21:21:49 -0600

    Mostrar nombre, direccion y telefono

---
+++ Nombre Direccion Telefono/src/nombre/direccion/telefono/NombreDireccionTelefono.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nombre.direccion.telefono;
+
+/**
+ *
+ * @author Tiffany
+ */
+public class NombreDireccionTelefono {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        String nombre = "Tiffany Chacòn", direccion = "El Chiltepe", telefono = "48740304";
+        System.out.println(nombre+"\n" + direccion +"\n" + telefono);
+    }
+    
+}
